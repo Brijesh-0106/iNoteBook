fa920923498f0ddbbbfd43938a8898b7ca52b04e
phenomenal <brijeshshah654@gmail.com>
2023-06-01 16:34:18 +0530

    this is commited after context api

---
+++ src/context/notes/noteState.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState = (props) => {
+
+    const s = {
+        name: "Phenomenal",
+        favPlayer: "kaneWilliamson"
+    }
+
+    const [state, setstate] = useState(s)
+
+    const update = () => {
+        setTimeout(() => {
+            setstate({
+                name: "Champion",
+                favPlayer: "Chris Hamsworth"
+            })
+        }, 2000);
+    }
+
+    return (
+        // {for javascript {for object} }
+        <NoteContext.Provider value={{ state: state, update: update }}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+export default NoteState;
